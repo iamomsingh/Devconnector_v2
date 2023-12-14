3dbf15bf3904cf58e7497fd6a4f537b1fbfdef20
om singh <iamomsingh11@gmail.com>
2023-12-14 10:48:51 +0530

    routing files created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("server is running...");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoute from "./routes/api/userRoute.js";
+import authRoute from "./routes/api/authRoute.js";
+import profileRoute from "./routes/api/profileRoute.js";
+import postsRoute from "./routes/api/postsRoute.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// all routes
+app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
+
+app.listen(PORT, () => console.log(`server running at port ${PORT}`));
